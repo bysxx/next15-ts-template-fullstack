8ceb4c768e926620139f77e5dca105357bde77f1
bysxx <0415white@gmail.com>
2025-09-15 12:32:49 +0900

    fix: get post without db

--- src/app/example/fetching/components/fetching-component.tsx
+++ src/app/example/fetching/components/fetching-component.tsx
@@ -1,13 +1,11 @@
-import { getPost } from "app/post/action";
-
 export default async function FetchingComponent() {
-  const data = await getPost();
-  const firstPost = data[0];
+  // const data = await getPost();
+  // const firstPost = data[0];
 
   return (
     <div className="">
-      <h1>{firstPost?.title}</h1>
-      <span>{firstPost?.description}</span>
+      {/* <h1>{firstPost?.title}</h1>
+      <span>{firstPost?.description}</span> */}
     </div>
   );
 }
